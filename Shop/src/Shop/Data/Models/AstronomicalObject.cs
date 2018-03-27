a6fa1da328f8830e01e1c7b79b83e96056178423
AlexSkorupsky <alex.skorupskyy@gmail.com>
2018-03-28 01:44:32 +0300

    Added base models

---
+++ Shop/src/Shop/Data/Models/AstronomicalObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Models
+{
+    public class AstronomicalObject
+    {
+        public int AstronomicalObjectId { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string LongDecription { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string ImageThumbnailUrl { get; set; }
+
+        public bool IsPreferedAstronomicalObject { get; set; }
+
+        public int InStock { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}

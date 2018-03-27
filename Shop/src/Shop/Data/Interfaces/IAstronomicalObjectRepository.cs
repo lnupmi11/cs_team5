8395df737a37b676efa7c1436081a4db6c70b8a1
AlexSkorupsky <alex.skorupskyy@gmail.com>
2018-03-28 01:47:35 +0300

    Added base repository interfaces

---
+++ Shop/src/Shop/Data/Interfaces/IAstronomicalObjectRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Shop.Data.Models;
+
+namespace Shop.Data.Interfaces
+{
+    interface IAstronomicalObjectRepository
+    {
+        IEnumerable<AstronomicalObject> AstronomicalObjects { get; set; }
+
+        IEnumerable<AstronomicalObject> PreferredAstronomicalObjects { get; set; }
+
+        AstronomicalObject GetAstronomicalObjectById(int AOId);
+    }
+}

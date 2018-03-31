c21197f91474e0f7201f1cf9794c2cb03797e51d
AlexSkorupsky <alex.skorupskyy@gmail.com>
2018-04-01 02:12:17 +0300

    Added AO controller

---
+++ Shop/src/Shop/Controllers/AstronomicalObjectController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using Shop.Data.Mocks;
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Shop.Data.Controllers
+{
+    public class AstronomicalObjectController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IAstronomicalObjectRepository _astronomicalObjectRepository;
+
+        public AstronomicalObjectController(ICategoryRepository categoryRepository, IAstronomicalObjectRepository astronomicalObjectRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _astronomicalObjectRepository = astronomicalObjectRepository;
+        }
+
+        public ViewResult List()
+        {
+            var astronomicalObjects = _astronomicalObjectRepository.AstronomicalObjects;
+            return View(astronomicalObjects);
+        }
+    }
+}

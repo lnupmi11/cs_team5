7658a2d67e18259eb9efb46e62ddd850ecdae9e3
MaryanTutunnyk <maryan.tutunnyk@gmail.com>
2018-05-04 11:43:54 +0300

    Add missing packages.

--- Shop/src/Shop/Data/AppDbContext.cs
+++ Shop/src/Shop/Data/AppDbContext.cs
@@ -1,8 +1,5 @@
-﻿using Shop.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Data.Models;
 
 namespace Shop.Data
 {
--- Shop/src/Shop/Startup.cs
+++ Shop/src/Shop/Startup.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Shop.Data.Interfaces;
-using Shop.Data.Models;
 using Shop.Data.Mocks;
 using Microsoft.Extensions.Configuration;
 using Shop.Data;

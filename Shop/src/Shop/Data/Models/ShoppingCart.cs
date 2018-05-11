833d967a0a602fff45dabb2086713c812c7c6502
AlexSkorupsky <alex.skorupskyy@gmail.com>
2018-05-11 23:04:17 +0300

    Added shopping cart model

---
+++ Shop/src/Shop/Data/Models/ShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Models
+{
+    public class ShoppingCart
+    {
+        public string ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+    }
+}
---
+++ Shop/src/Shop/Migrations/20180511194706_CartItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Shop.Migrations
+{
+    public partial class CartItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingCartItems",
+                columns: table => new
+                {
+                    ShoppingCartItemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Amount = table.Column<int>(nullable: false),
+                    AstronomicalObjectId = table.Column<int>(nullable: true),
+                    ShoppingCartId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCartItems", x => x.ShoppingCartItemId);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCartItems_AstronomicalObjects_AstronomicalObjectId",
+                        column: x => x.AstronomicalObjectId,
+                        principalTable: "AstronomicalObjects",
+                        principalColumn: "AstronomicalObjectId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCartItems_AstronomicalObjectId",
+                table: "ShoppingCartItems",
+                column: "AstronomicalObjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingCartItems");
+        }
+    }
+}

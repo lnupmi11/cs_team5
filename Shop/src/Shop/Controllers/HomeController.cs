d2edb390f1b30640755705d4cb4507a2a9ad730a
ivanternovyi <ivanternovyi@gmail.com>
2018-05-15 12:01:00 +0300

    Fixed import issues

--- Shop/src/Shop/Controllers/HomeController.cs
+++ Shop/src/Shop/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.ViewModels;
 
 namespace Shop.Controllers
 {
--- Shop/src/Shop/ViewModels/HomeViewModel.cs
+++ Shop/src/Shop/ViewModels/HomeViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Models;
 
 namespace Shop.ViewModels
 {

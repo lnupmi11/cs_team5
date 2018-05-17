b87000e325b41cf9d9974ef3a20c21e30fc37695
ivanternovyi <ivanternovyi@gmail.com>
2018-05-17 23:49:54 +0300

    Added models for ordering

---
+++ Shop/src/Shop/Data/Models/Order.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public List<OrderDetail> OrderLines { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public decimal OrderTotal { get; set; }
+
+        public DateTime OrderPlaced { get; set; }
+    }
+}

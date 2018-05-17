1b15dfbb1f393b33e99487ee09ed1302d9c437bd
ivanternovyi <ivanternovyi@gmail.com>
2018-05-18 01:44:11 +0300

    Fixed namespace error

--- Shop/src/Shop/Data/Models/OrderDetail.cs
+++ Shop/src/Shop/Data/Models/OrderDetail.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DrinkAndGo.Data.Models
+namespace Shop.Data.Models
 {
     public class OrderDetail
     {

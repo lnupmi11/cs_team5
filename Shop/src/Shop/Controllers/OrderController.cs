551e3bca59764c00a0d7390cfc648b5d9374db90
ivanternovyi <ivanternovyi@gmail.com>
2018-05-18 00:28:49 +0300

    Added order controller + view

---
+++ Shop/src/Shop/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        } 
+
+    }
+}
